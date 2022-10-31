a6c44442c0b6eb5eacfec2ba98929a7878f13b9c
Ricardo Pérez <ricardopp1992@gmail.com>
2022-10-31 10:45:48 -0300

    added remove counter icon

--- src/pages/Home/components/OptionsList/index.tsx
+++ src/pages/Home/components/OptionsList/index.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import type { FC } from 'react'
 import { View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 import { CounterStackScreens, DrawerMenuScreens, HomeNavigation, OptionListProps } from '@/interfaces'
-import { SecondaryButton } from '@/components/Buttons/SecondaryButton'
+import { PrimaryButton, SecondaryButton } from '@/components/Buttons'
 import { SimpleText } from '@/components/Texts'
 import styles from './styles'
 
@@ -23,16 +24,25 @@ const OptionsList: FC<OptionListProps> = ({ oldCounter, preservedId }) => {
     )
   }
 
+  const removeCounter = () => { }
+
   return (
-    <SecondaryButton style={styles.container} onPress={navigateToCounterScreen}>
-      <View>
+    <View style={styles.listRow}>
+      <SecondaryButton style={styles.container} onPress={navigateToCounterScreen}>
         {
           oldCounter.map(({ itemName, value }, i) => (
-            <SimpleText key={`${itemName}-${i}`}>{itemName}: {value}</SimpleText>
+            `${itemName} - ${value} ${i !== oldCounter.length - 1 ? '\n' : ''}`
+            // <SimpleText style={styles.text} key={`${itemName}-${i}`}>{itemName}: {value}</SimpleText>
           ))
         }
-      </View>
-    </SecondaryButton>
+      </SecondaryButton>
+      <PrimaryButton
+        onPress={removeCounter}
+        style={styles.closeButton}
+      >
+        <Icon style={styles.removeIcon} name="trash" />
+      </PrimaryButton>
+    </View>
   )
 }
 
--- src/pages/Home/components/OptionsList/styles.ts
+++ src/pages/Home/components/OptionsList/styles.ts
@@ -1,7 +1,30 @@
+import { normalTheme } from "@/theme";
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  listRow: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: normalTheme.primaryColor,
+    padding: 2,
+    marginVertical: 3,
+  },
   container: {
-    marginVertical: '3%'
+    borderWidth: 0,
+    flexWrap: 'nowrap',
+    flexDirection: 'column'
+  },
+  text: {
+    fontSize: 15,
+  },
+  closeButton: {
+    borderWidth: 0,
+  },
+  removeIcon: {
+    fontSize: 20,
   }
 })
